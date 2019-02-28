0e0506e5064ada800fdf6e690364c0b0f85dd9cc
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-02-28 17:56:57 -0500

    Added delete button to each Friend card. Added deleteFriend functionality using axios DELETE request.

--- friends/src/Friend.js
+++ friends/src/Friend.js
@@ -4,15 +4,22 @@ import './Friend.css';
 const Friend = props => {
   return (
     <div className="Friend">
-      <h1>{props.data.name}</h1>
+
+      <header>
+        <h1>{props.data.name}</h1>
+        <button onClick={e => props.deleteFriend(e, props.data.id)}>X</button>
+      </header>
+
       <div className="age">
         <h4>Age:</h4>
         <p>{props.data.age}</p>
       </div>
+
       <div className="email">
         <h4>email:</h4>
         <p>{props.data.email}</p>
       </div>
+
     </div>
   )
 }
